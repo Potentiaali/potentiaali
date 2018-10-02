8139a6c87ed66c18972fe8a2ee4337ea0b68f230
alehuo <aleksi.huotala@helsinki.fi>
2018-10-02 14:55:49 +0300

    Upgraded react-scripts to version 2.0.2

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const rewireSass = require('react-app-rewire-scss');
-
-/* config-overrides.js */
-module.exports = function override(config, env) {
-  config = rewireSass(config, env);
-  return config;
-}
