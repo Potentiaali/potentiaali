af32a84c4165bba8b49d34e69291c45370ed8592
alehuo <aleksi.huotala@helsinki.fi>
2019-02-14 21:27:48 +0200

    Refactor Nav menu to be a functional component & use hooks

--- src/components/partials/Nav.js
+++ src/components/partials/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
@@ -22,112 +22,55 @@ const menuMessages = defineMessages({
   }
 });
 
-class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuOpen: false
-    };
-  }
+const Nav = ({ lang, intl: { formatMessage }, setLanguage }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  /**
-   * Handle menu open & close
-   *
-   * @param {*} event
-   * @memberof App
-   */
-  handleClick = event => {
-    event.preventDefault();
-    this.setState(oldState => {
-      return {
-        menuOpen: !oldState.menuOpen
-      };
-    });
-  };
+  const menu = [
+    {
+      name: formatMessage(menuMessages.frontPage),
+      linkName: "",
+      link: "/",
+      disabled: false
+    },
+    {
+      name: formatMessage(menuMessages.schedule),
+      linkName: "schedule",
+      link: "/schedule",
+      disabled: false
+    },
+    {
+      name: formatMessage(menuMessages.subjects),
+      linkName: "subjects",
+      link: "/subjects",
+      disabled: false
+    }
+  ];
 
-  render() {
-    const {
-      lang,
-      intl: { formatMessage }
-    } = this.props;
-    const menu = [
-      {
-        name: formatMessage(menuMessages.frontPage),
-        linkName: "",
-        link: "/",
-        disabled: false
-      },
-      {
-        name: formatMessage(menuMessages.schedule),
-        linkName: "schedule",
-        link: "/schedule",
-        disabled: false
-      },
-      {
-        name: formatMessage(menuMessages.subjects),
-        linkName: "subjects",
-        link: "/subjects",
-        disabled: false
-      }
-    ];
-
-    return (
-      <nav className={styles.navigation}>
-        <div className={styles["navigation-container"]}>
-          <NavLink to="/" className={styles["nav-logo"]}>
-            <img
-              className=""
-              src="/logos/potentiaali-black.png"
-              alt="Kumpulan Potentiaali"
-            />
-          </NavLink>
-          <a
-            href="#menu"
-            className={styles["mobile-menu"]}
-            onClick={this.handleClick}
-          >
-            <span
-              className={
-                !this.state.menuOpen
-                  ? "fa fa-bars mobile-menu-icon"
-                  : "fa fa-close mobile-menu-icon"
-              }
-            />
-          </a>
-          {this.state.menuOpen && (
-            <div className={styles["mobile-nav-links"]}>
-              {menu &&
-                menu.map(
-                  menuItem =>
-                    !menuItem.disabled && (
-                      <NavLink
-                        exact
-                        to={menuItem.link}
-                        key={menuItem.linkName}
-                        activeClassName={"active-link"}
-                      >
-                        {menuItem.name}
-                      </NavLink>
-                    )
-                )}
-              {lang === "en" ? (
-                <button
-                  className="changeLanguage"
-                  onClick={() => this.props.setLanguage("fi")}
-                >
-                  Suomeksi
-                </button>
-              ) : (
-                <button
-                  className="changeLanguage"
-                  onClick={() => this.props.setLanguage("en")}
-                >
-                  In English
-                </button>
-              )}
-            </div>
-          )}
-          <div className={styles["nav-links"]}>
+  return (
+    <nav className={styles.navigation}>
+      <div className={styles["navigation-container"]}>
+        <NavLink to="/" className={styles["nav-logo"]}>
+          <img
+            className=""
+            src="/logos/potentiaali-black.png"
+            alt="Kumpulan Potentiaali"
+          />
+        </NavLink>
+        <a
+          href="#menu"
+          className={styles["mobile-menu"]}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span
+            className={
+              !menuOpen
+                ? "fa fa-bars mobile-menu-icon"
+                : "fa fa-close mobile-menu-icon"
+            }
+          />
+        </a>
+        {menuOpen && (
+          <div className={styles["mobile-nav-links"]}>
             {menu &&
               menu.map(
                 menuItem =>
@@ -144,25 +87,56 @@ class Nav extends React.Component {
               )}
             {lang === "en" ? (
               <button
-                className={styles.changeLanguage}
-                onClick={() => this.props.setLanguage("fi")}
+                className="changeLanguage"
+                onClick={() => setLanguage("fi")}
               >
                 Suomeksi
               </button>
             ) : (
               <button
-                className={styles.changeLanguage}
-                onClick={() => this.props.setLanguage("en")}
+                className="changeLanguage"
+                onClick={() => setLanguage("en")}
               >
                 In English
               </button>
             )}
           </div>
+        )}
+        <div className={styles["nav-links"]}>
+          {menu &&
+            menu.map(
+              menuItem =>
+                !menuItem.disabled && (
+                  <NavLink
+                    exact
+                    to={menuItem.link}
+                    key={menuItem.linkName}
+                    activeClassName="active-link"
+                  >
+                    {menuItem.name}
+                  </NavLink>
+                )
+            )}
+          {lang === "en" ? (
+            <button
+              className={styles.changeLanguage}
+              onClick={() => setLanguage("fi")}
+            >
+              Suomeksi
+            </button>
+          ) : (
+            <button
+              className={styles.changeLanguage}
+              onClick={() => setLanguage("en")}
+            >
+              In English
+            </button>
+          )}
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 Nav.propTypes = {
   intl: PropTypes.any,
