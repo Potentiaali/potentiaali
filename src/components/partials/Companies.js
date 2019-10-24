e521ff773a31ead658b9e9c0c8977aeb2da615ad
alehuo <aleksi.huotala@helsinki.fi>
2019-10-24 17:37:28 +0300

    UI tweaks

--- src/components/partials/Companies.js
+++ src/components/partials/Companies.js
@@ -2,12 +2,17 @@ import React from "react";
 import companies from "../../data/companies.json";
 import style from "./Companies.module.scss";
 import { Localized } from "fluent-react/compat";
+import { Textfit } from "react-textfit";
 
 const Companies = () => (
   <>
-    <Localized id="companies-title-text">
-      <h1>Vuoden 2019 Kumpulan Potentiaali -tapahtuman yritykset</h1>
-    </Localized>
+    <Textfit mode="single" max={20}>
+      <h1>
+        <Localized id="companies-title-text">
+          Vuoden 2019 Kumpulan Potentiaali -tapahtuman yritykset
+        </Localized>
+      </h1>
+    </Textfit>
     <div className={style.companies}>
       {companies
         .sort((a, b) => a.alt.localeCompare(b.alt))
