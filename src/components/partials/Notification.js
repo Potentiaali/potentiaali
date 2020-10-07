3200eac6dc04ee9f1679734c419cffc8ab86e722
alehuo <aleksi.huotala@helsinki.fi>
2020-10-07 10:26:34 +0300

    Update website to 2020 event info. Fix vulnerabilities.

---
+++ src/components/partials/Notification.js
@@ -0,0 +1,23 @@
+import React from "react";
+import propTypes from "prop-types";
+import styles from "./Notification.module.scss";
+import classNames from "classnames";
+
+const Notification = ({ type = "info", title, children }) => (
+  <div
+    className={classNames(styles.notification, styles[`${type}-notification`])}
+  >
+    {title !== undefined && (
+      <div className={styles.notificationTitle}>{title}</div>
+    )}
+    <div className={styles.notificationContents}>{children}</div>
+  </div>
+);
+
+Notification.propTypes = {
+  type: propTypes.string,
+  children: propTypes.any.isRequired,
+  title: propTypes.any.isRequired
+};
+
+export default Notification;
