2e5b576c9f91597122b4c8689a9a56d2bbaa6522
Jere Lahelma <je-l@users.noreply.github.com>
2023-11-30 11:59:21 +0200

    Remove unused redux actions and action types

--- src/reducers/CompanyReducer.js
+++ src/reducers/CompanyReducer.js
@@ -5,21 +5,8 @@ const initialState = {
   companies: companyList
 }
 
-const ScheduleTypes = {
-  SET_COMPANIES: "SET_COMPANIES"
-}
-
-export const setCompanies = (companies) => ({
-  type: ScheduleTypes.SET_COMPANIES,
-  payload: {
-    companies
-  }
-})
-
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ScheduleTypes.SET_COMPANIES:
-      return { ...state, companies: { ...action.payload.companies } }
     default:
       return state
   }
--- src/reducers/ScheduleReducer.js
+++ src/reducers/ScheduleReducer.js
@@ -374,21 +374,8 @@ const initialState = {
   events: schedule
 }
 
-const ScheduleTypes = {
-  SET_EVENTS: 'SET_EVENTS'
-}
-
-export const setEvents = (events) => ({
-  type: ScheduleTypes.SET_EVENTS,
-  payload: {
-    events
-  }
-})
-
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ScheduleTypes.SET_EVENTS:
-      return { ...state, events: { ...action.payload.events } }
     default:
       return state
   }
