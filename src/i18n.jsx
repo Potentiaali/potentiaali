9f5a47233edf33e731291b996bcbdc715565488f
Jere Lahelma <je-l@users.noreply.github.com>
2023-11-30 11:42:02 +0200

    Move translations to src directory

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,10 +1,10 @@
 import i18n from 'i18next'
 // import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from 'react-i18next'
-import fi from '../public/locales/fi/translation.json'
-import en from '../public/locales/en/translation.json'
-import fiSubjects from '../public/locales/fi/subjects.json'
-import enSubjects from '../public/locales/en/subjects.json'
+import fi from './locales/fi/translation.json'
+import en from './locales/en/translation.json'
+import fiSubjects from './locales/fi/subjects.json'
+import enSubjects from './locales/en/subjects.json'
 
 i18n
   //  .use(LanguageDetector)
