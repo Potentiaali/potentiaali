fa28f41e51d8c0da663ccb7b44f108b6c2b667bd
Jere Lahelma <je-l@users.noreply.github.com>
2023-11-30 11:34:47 +0200

    Use json imports for translations
    The advantage of this is that when there are changes to translations,
    they shouldn't be cached anymore, and instead they should be immediately
    visible.

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,10 +1,12 @@
 import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
 // import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from 'react-i18next'
+import fi from '../public/locales/fi/translation.json'
+import en from '../public/locales/en/translation.json'
+import fiSubjects from '../public/locales/fi/subjects.json'
+import enSubjects from '../public/locales/en/subjects.json'
 
 i18n
-  .use(Backend)
   //  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -20,6 +22,17 @@ i18n
 
     interpolation: {
       escapeValue: false
+    },
+    resources: {
+      fi: {
+        translation: fi,
+        subjects: fiSubjects
+      },
+      en: {
+        translation: en,
+        subjects: enSubjects
+
+      },
     }
   })
 
